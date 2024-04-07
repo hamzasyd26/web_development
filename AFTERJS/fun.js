6761152eca9a5be73ec4621edf8e12b8d694fbd9
Hamza Sayyed <hamzasayyed126@gmail.com>
2024-04-07 19:23:38 +0530

    learned API

---
+++ AFTERJS/fun.js
@@ -0,0 +1,19 @@
+// const a = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+
+// const a2 = a.map(function (s) {
+// 	return s.length;
+// });
+
+// console.log("Normal way ", a2); // [8, 6, 7, 9]
+
+// const a3 = a.map((s) => s.length);
+
+// console.log("Using Arrow Function ", a3); // [8, 6, 7, 9]
+
+// let array = [1,2,3,3];
+// console.log(array);
+
+let arr= new Array(1,2,3,4,5);
+console.log(arr);
+
+console.log(arr.splice(1,2));
